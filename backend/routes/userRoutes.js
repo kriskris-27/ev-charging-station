8c282f8238b980ed5f3490016f26becc0280de9f
Kris <krisnakumars0101@gmail.com>
2025-02-22 14:13:26 +0530

    test 8

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,18 @@
-const express = require("express")
-const verifyToken=require("../middlewares/authMiddleware")
-const authorizeRoles=require("../middlewares/roleMIddleware")
+const express = require('express');
 const router = express.Router();
+const verifyToken = require("../middlewares/authMiddleware");
+const roleMiddleware = require('../middlewares/roleMiddleware'); // Ensure the file name and path are correct
 
 // only super-admin aka kris
-router.get("/superadmin",verifyToken,authorizeRoles("superadmin"),(req,res) => {
-    res.json({message:"WELCOME KRIS"})
+router.get("/superadmin", verifyToken, roleMiddleware("superadmin"), (req, res) => {
+    res.json({ message: "WELCOME KRIS" });
 });
+
 //Both admin and kris
 // router.get("/admin",verifyToken,authorizeRoles("superadmin","admin"),(req,res) => {
 //     res.json({message:"WELCOME ADMINS"})
 // });
+
 //All can access
 // router.get("/user",verifyToken,authorizeRoles("superadmin","admin","user"),(req,res) => {
 //     res.json({message:"HELLO USER"})
